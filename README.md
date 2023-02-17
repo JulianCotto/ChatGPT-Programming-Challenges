e7b1be15cf33c1e70d3ba9b4a7f4c77a50181c54
juliancotto <julian.cotto@g.austincc.edu>
2023-02-16 23:17:20 -0600

    Complete

---
+++ README.md
@@ -0,0 +1,15 @@
+# Asked ChatGPT For A Programminmg Assignment
+ChatGPT's response: 
+
+Sure, here's a simple C++ problem you can try to solve:
+
+Write a C++ program that takes an integer input `n` and prints the first `n` even numbers in reverse order.
+
+For example, if the input is 5, the output should be: 10 8 6 4 2
+
+Here's some additional information and requirements for the program:
+
+You can assume that the input is a positive integer.
+You can use any loop constructs you like to solve this problem.
+Your program should be able to handle large values of n (e.g. up to 1,000,000).
+Good luck!
---
+++ main.cpp
@@ -0,0 +1,32 @@
+/*********************
+Name: Julian Cotto
+Assignment 4 - Int Stack
+Purpose: contains user facing interactions
+**********************/
+
+#include "main.h"
+
+
+int main()
+{
+    int num;
+
+    cout << "Enter a number" << endl;
+    cin >> num;
+
+    while(num < 1) { // check if the number is valid
+        cout << "Error: " << num << " is not a valid number" << endl;
+        cout << "Enter a number" << endl;
+        cin >> num;
+    }
+
+    vector<int> evens = Reverse::reverseEvens((Reverse::getEvens(num))); // call the reverseEvens method
+    for(int even : evens) { // print the reversed evens
+        cout << even << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
